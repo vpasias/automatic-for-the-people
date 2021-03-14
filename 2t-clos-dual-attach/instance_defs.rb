68dfa7cd871317d00420f844027e8adf80c837d8
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-14 13:45:56 +0200

    Update instance_defs.rb

--- 2t-clos-dual-attach/instance_defs.rb
+++ 2t-clos-dual-attach/instance_defs.rb
@@ -1,9 +1,9 @@
 NUM_SPINES = 2
-NUM_LEAVES = 4
+NUM_LEAVES = 6
 NUM_EXIT_LEAVES = 2
 NUM_DCEDGE_RTRS = 1             # Hardcoded
 NUM_FIREWALL = 1                # Hardcoded
-NUM_SERVERS_PER_LEAF = 2
+NUM_SERVERS_PER_LEAF = 3
 
 SPINE_NOS = "cumulus"
 LEAF_NOS = "cumulus"
